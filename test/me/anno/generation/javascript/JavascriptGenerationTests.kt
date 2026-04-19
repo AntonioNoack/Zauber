28208efcd3780f69b9b18d49f3f75c16f2b28feb
Antonio Noack <antonio-noack@gmx.de>
2026-04-19 16:23:53 +0200

    Planning JS+WASM a little

---
+++ test/me/anno/generation/javascript/JavascriptGenerationTests.kt
@@ -0,0 +1,13 @@
+package me.anno.generation.javascript
+
+import org.junit.jupiter.api.Test
+
+class JavascriptGenerationTests {
+    @Test
+    fun testHelloWorld() {
+        // todo define zauber code
+        // todo automatically import all Javascript bindings from d.ts?
+        // todo call into those functions :3
+
+    }
+}
---
+++ test/me/anno/generation/wasm/WASMGenerationTests.kt
@@ -0,0 +1,13 @@
+package me.anno.generation.wasm
+
+import org.junit.jupiter.api.Test
+
+class WASMGenerationTests {
+    @Test
+    fun testHelloWorld() {
+        // todo define zauber code
+        // todo we don't have any bindings...
+        //  should we a) just import them from JavaScript and create our own? seems sensible :)
+
+    }
+}
